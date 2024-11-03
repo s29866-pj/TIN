5cf23dd1dafc753dc94f81bdd14370d1241acc59
Aleks277 <aleks277@myMacbook.local>
2024-11-03 12:56:09 +0100

    Zadania css

---
+++ zajecia2/Zadanie2/main.js
@@ -0,0 +1,5 @@
+var dropdown = document.querySelectorAll(".dropdown");
+var menu = document.querySelectorAll(".main");
+dropdown.addEventListener("hover", function() {
+  menu.style.backroundColor = "red";
+});
